eff183acfe84dc41a0688184df7c2e8dadb73d4c
y2k01 <scotlandfjord@protonmail.com>
2021-10-05 10:36:03 +0900

    [refactoring] css loader to styled component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import styles from './Loader.module.css'
+import styled, {keyframes} from "styled-components";
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const LdsDualRing = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  &:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #000;
+    border-color: #000 transparent #000 transparent;
+    animation: ${spin} 1.2s linear infinite;
+  }
+`
 
 const Loader = () => {
     return (
-        <div className={styles.ldsDualRing}></div>
+        <LdsDualRing />
     );
 };
 
