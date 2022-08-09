ed93e29407cbd4ef37077d57e97b28e0c2e331cf
aaltoo <scotlandfjord@protonmail.com>
2022-08-09 22:45:28 +0900

    Add search icon to input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,32 +2,38 @@ import React, { FormEvent, useState } from 'react'
 import styled from 'styled-components'
 import { useAppDispatch } from '../app/hooks'
 import { fetchCityWeather } from '../features/app-slice'
+import searchIcon from '../assets/icons/search-icon.svg'
 
 const Section = styled.section`
-    color: #FFF;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Form = styled.form`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 2rem 0 2rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem 0 2rem 0;
 `
 
 const Input = styled.input`
-    padding: 1rem 0 1rem 0;
-    width: 200px;
-    height: 50px;
-    outline: none;
-    border: none;
-    background-color: #f2f2f2;
-    border-radius: 20px;
-    padding: 5px;
+  padding: 1rem 0 1rem 0;
+  width: 200px;
+  height: 50px;
+  outline: none;
+  border: none;
+  border-radius: 20px;
+  padding: 5px;
+  padding-left: 25px;
+  font-family: inherit;
+  font-size: inherit;
+  background-image: url(${searchIcon});
+  background-repeat: no-repeat;
+  background-position: 5px;
 `
 
 const SearchBar: React.FC = () => {
@@ -41,11 +47,18 @@ const SearchBar: React.FC = () => {
   const [inputField, setInputField] = useState('')
 
   return (
-        <Section>
-            <Form onSubmit={commenceSearch}>
-                <Input type="text" value={inputField} onChange={(e: React.FormEvent<HTMLInputElement>) => setInputField(e.currentTarget.value)}/>
-            </Form>
-        </Section>
+    <Section>
+      <Form onSubmit={commenceSearch}>
+        <Input
+          type="text"
+          placeholder="Search..."
+          value={inputField}
+          onChange={(e: React.FormEvent<HTMLInputElement>) =>
+            setInputField(e.currentTarget.value)
+          }
+        />
+      </Form>
+    </Section>
   )
 }
 
