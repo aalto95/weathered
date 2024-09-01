cd6bcb22ed152aae885f6a24596cc04283f0b9af
aalto95 <scotlandfjord@protonmail.com>
2024-09-01 20:40:47 +0900

    Migrate from CRA to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './app/store'
@@ -7,13 +7,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import './i18n'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
